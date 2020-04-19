ffdf7c8e497ac0d9a89d34b7808f51414e5d84fb
Ohlamesaint <mkop9456@gmail.com>
2020-04-19 14:00:12 +0800

    [JOIN] Test Wrong

--- js/main.js
+++ js/main.js
@@ -12,16 +12,25 @@ $(document).ready(function(){
             }).then(res=>{
                 if(!res.data.signin){    //可以順便獲得會員profile
                     console.log(res);
-                    console.log("789");
-                    setTimeout(() => {
-                        window.location.replace('https://ohlamesaint.github.io/TuanTuanGOFrontend/pages/login.html');
-                    }, );
+                    console.log(res);
+                    $("body").addClass("modal-open");
+                    $("#loginPrompt").addClass("show").css("display", "block");
+                    $("#not").on('click', ()=>{
+                        $("body").removeClass("modal-open");
+                        $("#loginPrompt").removeClass("show").css("display", "none");
+                    })
+                    $("#yes").on('click', ()=>{
+                        setTimeout(() => {
+                            window.location.replace('https://luffy.ee.ncku.edu.tw/~Shang/TuanTuanGO/pages/login.html');
+                        }, );
+                    })
+                    // console.log("789");
                 }else{
                     console.log(res);
                     console.log("456");
                     console.log(belowBar[i].children[1].textContent);
                     localStorage.setItem("target", i)
-                    window.location.replace('https://ohlamesaint.github.io/TuanTuanGOFrontend/pages/func.html');
+                    window.location.replace('https://luffy.ee.ncku.edu.tw/~Shang/TuanTuanGO/pages/func.html');
                 }
             }).catch(err=>{
                 console.log("123")
@@ -38,8 +47,17 @@ $(document).ready(function(){
         }).then(res=>{
             if(!res.data.signin){    //可以順便獲得會員profile
                 console.log(res);
-                // $("body").addClass("modal-open");
-                // $("#loginPrompt").addClass("show").css("display", "block");
+                $("body").addClass("modal-open");
+                $("#loginPrompt").addClass("show").css("display", "block");
+                $("#not").on('click', ()=>{
+                    $("body").removeClass("modal-open");
+                    $("#loginPrompt").removeClass("show").css("display", "none");
+                })
+                $("#yes").on('click', ()=>{
+                    setTimeout(() => {
+                        window.location.replace('https://luffy.ee.ncku.edu.tw/~Shang/TuanTuanGO/pages/login.html');
+                    }, );
+                })
                 // setTimeout(() => {
                 //     window.location.replace('./pages/login.html');
                 // }, );
@@ -49,7 +67,7 @@ $(document).ready(function(){
                 // localStorage.setItem("target", 4)
                 $(tuanGoer).addClass("tuanGoeranimate");
                 tuanGoer.addEventListener("animationend", ()=>{
-                    window.location.replace('https://ohlamesaint.github.io/TuanTuanGOFrontend/pages/tuanGo.html');
+                    window.location.replace('https://luffy.ee.ncku.edu.tw/~Shang/TuanTuanGO/pages/tuanGo.html');
                 })
             }
         }).catch(err=>{

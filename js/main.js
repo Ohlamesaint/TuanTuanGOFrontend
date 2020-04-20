67ad724394b91c4bf9603413f321a68fea7735bd
Ohlamesaint <mkop9456@gmail.com>
2020-04-20 17:22:29 +0800

    [JOIN] Test Wrong

--- js/main.js
+++ js/main.js
@@ -109,9 +109,9 @@ $(document).ready(function(){
         document.querySelector("#TuanGOerJoinPageWrap>#TuanGOType").textContent = TuanGOInform.TuanGOType?'unpack':'promote';
         if(TuanGOInform.TuanGOType){
             // unpack
-            document.querySelector("#unpackedTabInJoin").classList.add('show')
+            document.querySelector("#unpackedTabInJoin").classList.add('active')
         } else{
-            document.querySelector("#promoteTabInJoin").classList.add('show')
+            document.querySelector("#promoteTabInJoin").classList.add('active')
         }
         document.querySelector("#JoinPageTopNav").classList.add('show');
         document.querySelector("#TuanGOerJoinPageWrap").classList.add('show');

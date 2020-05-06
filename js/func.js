b4e0ef7e8b84643f11ec3eba16c21ba9a72a6ec8
eric5800602 <32606327+eric5800602@users.noreply.github.com>
2020-05-06 14:36:59 +0800

    [PAGES]Modify join page

--- js/func.js
+++ js/func.js
@@ -825,8 +825,8 @@ $(document).ready(function(){
                             document.querySelector("#JoinTuanGOTuanGOType").textContent = transfer_list[i].TuanGOType?'unpack':'promote';
                             document.querySelector("#JoinTuanGOExpirationDate").textContent = new Date(transfer_list[i].ExpirationTime).toString().slice(0, 24);
                             document.querySelector("#JoinTuanGOCost").textContent = transfer_list[i].disccountPrice +　"$ /per";
-                            document.querySelector("JoinTuanGOContractAddress").textContent = String(transfer_list[i].contract_address);
                             console.log(transfer_list[i].contract_address);
+                            document.querySelector("JoinTuanGOContractAddress").textContent = String(transfer_list[i].contract_address);
                             var num = 0;
                             var TuanGOerLine = "";
                             num = transfer_list[i].SoldAmounts;

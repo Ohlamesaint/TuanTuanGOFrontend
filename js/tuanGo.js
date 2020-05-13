365cc916e4fb1fd66eb6d00616806298808bb0de
Ohlamesaint <mkop9456@gmail.com>
2020-05-13 16:12:58 +0800

    [TuanGO] conclusion

--- js/tuanGo.js
+++ js/tuanGo.js
@@ -476,12 +476,12 @@ $(document).ready(()=>{
                         const goto = document.querySelectorAll('#conclusionInform>a')
                         goto[0].addEventListener('click', (e)=> {
                             e.preventDefault();
-                            localStorage.setItem('tagetPage', 2)
+                            localStorage.setItem('taget', 2)
                             window.location.replace('./func.html');
                         });
                         goto[1].addEventListener('click', (e) => {
                             e.preventDefault();
-                            localStorage.setItem('targetPage', 1);
+                            localStorage.setItem('target', 1);
                             window.location.replace('./func.html');
                         });
                     }).catch((err)=>{

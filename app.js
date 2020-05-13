dd65999916e56a7d838ed4812abb21fa11f78588
Ohlamesaint <mkop9456@gmail.com>
2020-05-13 21:26:18 +0800

    [PWA] Notification

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
 if("serviceWorker" in navigator){
     navigator.serviceWorker.register("./sw.js")
-        .then((reg)=>console.log("service worker registered", reg))
-        .catch((err)=>console.log(new Error("service worker not registered"), err))
+    .then((reg) => {
+        console.log("service worker registered", reg) 
+        
+        let enableNotifications = document.querySelectorAll('.enable-notification');
+        
+        if('Notification' in window) {
+            for(let i=0; i<enableNotifications.length; i++){
+                enableNotifications[i].style.display = 'inline-block';
+                enableNotifications[i].addEventListener('click', askForNotificationPermission);
+            }
+        }
+    }).catch((err) => {
+        console.log(new Error("service worker not registered"), err)
+    })
+}
+
+function askForNotificationPermission() {
+    Notification.requestPermission((status) => {
+        console.log('User choice', status);
+        if(status!=='granted'){
+            console.log('user denied');
+        } else {
+            
+        }
+    })
 }

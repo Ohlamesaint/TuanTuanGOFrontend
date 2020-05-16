f485db8c60a8ba5d5c17f45122a9684f9b182bf4
b112137 <e0010209@gmail.com>
2020-05-16 18:06:35 +0800

    [PAGES] Bluetooth connect

--- js/func.js
+++ js/func.js
@@ -1063,6 +1063,7 @@ function handleLedStatusChanged(event) {
       else if(over_count == over_num){
         file_string += json_string;
         console.log(file_string);
+        file_string = JSON.stringify(file_string);
         console.log(file_string[0]["time"]);
         var data_len = Object.keys(file_string).length
         var data_time = [];

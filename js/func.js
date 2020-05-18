dc9924bdbee069bdf8350853eb54c5380255f7e3
Ohlamesaint <mkop9456@gmail.com>
2020-05-18 12:31:46 +0800

    [PHOTO] enable photo download and upload

--- js/func.js
+++ js/func.js
@@ -194,7 +194,7 @@ $(document).ready(function () {
             document.querySelector("#email").textContent = res.data.data.email;
             document.querySelector("#address").textContent = res.data.data.address;
             document.querySelector("#region").textContent = res.data.data.region+', Taiwan';
-            document.querySelector("#headPaste").setAttribute('style', `backgound: url('"${res.data.data.headPaste}"')`)
+            document.querySelector("#headPaste").setAttribute('style', `backgound-image: url("${res.data.data.headPaste}")`)
             // var blob = new Blob([res.data.headPaste], { type: "image/jpg" });
             // console.log(blob);
             // var urlCreator = window.URL || window.webkitURL;

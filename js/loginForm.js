bc0f89fcc3fd893012a598533613ddc50c470d89
Ohlamesaint <mkop9456@gmail.com>
2020-05-18 11:51:38 +0800

    [PHOTO] enable photo download and upload

--- js/loginForm.js
+++ js/loginForm.js
@@ -27,10 +27,10 @@ $(document).ready(function(){
     $(signinLoading).hide();
     submit.addEventListener("click", (event)=>{
         event.preventDefault();
-        login.username = document.querySelector("#username").value;
+        login.username = document.querySelector("#account").value;
         login.password = document.querySelector("#password").value;
         if(!login.username){
-            document.querySelector("#usernameWrong").textContent = "請輸入會員帳號!";
+            document.querySelector("#accountWrong").textContent = "請輸入會員帳號!";
         }
         else if(!login.password){
             document.querySelector("#passwordWrong").textContent = "請輸入帳號密碼!"
@@ -49,7 +49,7 @@ $(document).ready(function(){
             .then(function (response) {
                 localStorage.setItem('token', response.data.data.token);
                 document.querySelector("#passwordWrong").textContent = "";
-                document.querySelector("#usernameWrong").textContent = "";
+                document.querySelector("#accountWrong").textContent = "";
                 signinLoading.setAttribute("style", "opacity: 0.8; background-color: white");
                 loading.setAttribute("style", "opacity: 0.0;");
                 signinLoading.addEventListener("transitionend", ()=>{
@@ -70,7 +70,7 @@ $(document).ready(function(){
         }
     })
     accountTextfield.addEventListener("focus", ()=>{
-        document.querySelector("#usernameWrong").textContent = "";
+        document.querySelector("#accountWrong").textContent = "";
     });
     passwordTextfield.addEventListener("focus", ()=>{
         document.querySelector("#passwordWrong").textContent = "";

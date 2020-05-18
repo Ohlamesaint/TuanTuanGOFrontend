8339e31784373ae03352cd1695dc1e249a159040
Ohlamesaint <mkop9456@gmail.com>
2020-05-18 17:12:09 +0800

    [TUANGO] debug

--- js/tuanGo.js
+++ js/tuanGo.js
@@ -240,6 +240,7 @@ $(document).ready(()=>{
             title.textContent = "2. TuanGO Setting";
             $(tuanGoer[1]).addClass('firstMove');
             tuanGoer[1].addEventListener('transitionend', showEventTwo, false)          //第二個畫面出現
+            document.querySelector(".topPhoto").style.backgroundImage = `url("${productImform.data[0].productPhoto}")`
             document.querySelector(".name").textContent = productInform.data[0].productName;
             document.querySelector("input[name=promoteExpiration]").min = moment().format('YYYY')+"-"+moment().format('MM')+"-"+(parseInt(moment().format('DD'))+1).toString()+"T"+moment().format('hh')+":"+moment().format('mm');
             document.querySelector("input[name=promoteExpiration]").value = moment().format('YYYY')+"-"+moment().format('MM')+"-"+(parseInt(moment().format('DD'))+1).toString()+"T"+moment().format('hh')+":"+moment().format('mm');
@@ -288,7 +289,7 @@ $(document).ready(()=>{
                 })
                 document.querySelector("#promoteProductNum").addEventListener("change", ()=>{
                     console.log($("#promoteProductNum").val());
-                    console.log(productDetail.data[0].promotionlowestNum);
+                    console.log(productDetail.data[0].promotionLowestNum);
                     if($("#promoteProductNum").val() > productDetail.data[0].promotionLowestNum){
                         $("#promoteWarningText").text("您超過了最多可選擇的數量!");
                         $(footer[1]).children().css("color", "white");

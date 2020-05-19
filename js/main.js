c8894b81ed27127171dcbcd575939609c39496e8
Ohlamesaint <mkop9456@gmail.com>
2020-05-19 14:45:46 +0800

    [TUANGO] debug

--- js/main.js
+++ js/main.js
@@ -84,7 +84,7 @@ $(document).ready(function(){
         document.querySelector("#JoinPageTopNav").classList.remove('show')
         document.querySelector("#footerInJoin").classList.remove('show')
         document.querySelector("#promoteTabInJoin").classList.remove('active')
-        document.querySelector("body").removeAttribute('overflow');
+        document.querySelector("body").style.overflow('');
     })
     document.querySelector("#TuanGOerJoinPage").addEventListener("transitionend", (e)=>{
         e.preventDefault();

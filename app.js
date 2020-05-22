7bf3224ab18e9295ad30e5e84d73cacfc0f0f743
Ohlamesaint <mkop9456@gmail.com>
2020-05-22 13:18:23 +0800

    [SUBSCRIPTION] get new subscription

--- app.js
+++ app.js
@@ -1,34 +1,5 @@
 const vapidKey = 'BGxHf6ZQkHVoIdROO4Fir61eouPlqUp3IzxsV4ud10FeXgS5vvG9q3Gw5J7lsp2XHnF_49aJ9RxWNV99_TD9--8';
 
-const send = async () => {
-    try{
-        
-        console.log(123);
-        const userChoice = askForNotificationPermission();
-        setPushSubcribe();
-        // console.log(userChoice);
-        // console.log(123)
-        // if(userChoice){
-        //     const subscription = await register.pushManager.subscribe({
-        //         userVisibleOnly: true,
-        //         applicationServerKey: urlBase64ToUint8Array(vapidKey)
-        //     })
-        //     console.log(JSON.stringify(subscription))
-        //     await fetch('https://tuantuango.herokuapp.com/subscribe', {
-        //     method: 'POST',
-        //     headers: {
-        //         'content-type': 'application/json'
-        //     },
-        //     body: JSON.stringify(subscription) 
-        //     })
-        // } else {
-        //     console.log('user denied');
-        // }
-    } catch (err) {
-        console.log('something went wrong => app => send', err);
-    }
-}
-
 if("serviceWorker" in navigator){
     navigator.serviceWorker.register('./sw.js')
     .then(res => {
@@ -36,40 +7,45 @@ if("serviceWorker" in navigator){
     }).catch(err => {
         console.error(err);
     });
-    // navigator.serviceWorker.register("./sw.js")
-    // .then((reg) => {
-    //     console.log("service worker registered", reg) 
-    //     let enableNotifications = document.querySelectorAll('.enable-notification');
-    //     if('Notification' in window) {
-    //         for(let i=0; i<enableNotifications.length; i++){
-    //             enableNotifications[i].style.display = 'inline-block';
-    //             enableNotifications[i].addEventListener('click', askForNotificationPermission);
-    //         }
-    //     }
-    // }).catch((err) => {
-    //     console.log(new Error("service worker not registered"), err)
-    // })
 }
 
+// const send = async () => {
+//     try{
+        
+//         console.log(123);
+//         const userChoice = askForNotificationPermission();
+//         setPushSubcribe();
+//         // console.log(userChoice);
+//         // console.log(123)
+//         // if(userChoice){
+//         //     const subscription = await register.pushManager.subscribe({
+//         //         userVisibleOnly: true,
+//         //         applicationServerKey: urlBase64ToUint8Array(vapidKey)
+//         //     })
+//         //     console.log(JSON.stringify(subscription))
+//         //     await fetch('https://tuantuango.herokuapp.com/subscribe', {
+//         //     method: 'POST',
+//         //     headers: {
+//         //         'content-type': 'application/json'
+//         //     },
+//         //     body: JSON.stringify(subscription) 
+//         //     })
+//         // } else {
+//         //     console.log('user denied');
+//         // }
+//     } catch (err) {
+//         console.log('something went wrong => app => send', err);
+//     }
+// }
 
 
 
 
 
-function urlBase64ToUint8Array(base64String) {
-    const padding = '='.repeat((4 - base64String.length % 4) % 4);
-    const base64 = (base64String + padding)
-    .replace(/-/g, '+')
-    .replace(/_/g, '/');
-    
-    const rawData = window.atob(base64);
-    const outputArray = new Uint8Array(rawData.length);
-    
-    for (let i = 0; i < rawData.length; ++i) {
-        outputArray[i] = rawData.charCodeAt(i);
-    }
-    return outputArray;
-}
+
+
+
+
 
 
 
